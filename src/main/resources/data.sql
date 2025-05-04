c7980e9c53c89f52a9914abfb3e43821a1bc8352
Guilhermy Rodrigues <guilhermyr87@gmail.com>
2025-05-04 14:08:49 -0300

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.produto
+(id, descricao, imagem_url, nome, preco, tempo_preparo_minutos)
+VALUES(1, 'Expresso preparado com grãos selecionados, mocha house, leite, e sabor de caramelo. 400m', 'https://i.imgur.com/VVbv1CP.jpeg', 'Latte', 5.90, 3);
+
+INSERT INTO public.produto
+(id, descricao, imagem_url, nome, preco, tempo_preparo_minutos)
+VALUES(2, 'Expresso encorpado com creme de leite, caramelo e mocha house. 400ml', 'https://i.imgur.com/0onY4m9.png', 'Caramelo Macchiato', 10.90, 8);
+
+INSERT INTO public.produto
+(id, descricao, imagem_url, nome, preco, tempo_preparo_minutos)
+VALUES(3, 'Um refresco de sabores com erva-mate levemente adocicado, framboesa, hibisco e frutas...', 'https://i.imgur.com/pGnhYOW.jpeg', 'Chá de frutas vermelhas', 6.90, 5);
+
+INSERT INTO public.produto
+(id, descricao, imagem_url, nome, preco, tempo_preparo_minutos)
+VALUES(4, 'Seleção de morangos frescos e chantilly, bebida cremosa e saborosa. 500ml', 'https://i.imgur.com/N0bktpH.png', 'Morango Salty Cream', 13.90, 10);
+
+INSERT INTO public.produto
+(id, descricao, imagem_url, nome, preco, tempo_preparo_minutos)
+VALUES(5, 'Delicioso milkshake de morango com cobertura cremosa. 500ml', 'https://i.imgur.com/QSxpec8.png', 'Shake de Morango', 15.90, 12);
+
+INSERT INTO public.produto
+(id, descricao, imagem_url, nome, preco, tempo_preparo_minutos)
+VALUES(6, 'Bebida cremosa com leite, chocolate e crocantes de Ovomaltine. 500ml', 'https://i.imgur.com/KjzPfD1.png', 'Shake de Ovomaltine', 15.90, 12);
